fd910d61d0a1328f123808206f07b4c08cdf312d
Marcelo Junqueira <marcelojxt@gmail.com>
2024-03-06 14:15:22 -0300

    [M03S06] Ex 3 - Mini-Projeto Avaliações - Implementando a notificação por e-mail

---
+++ src/main/java/com/avalialivros/m3s04/operations/EmailNotification.java
@@ -0,0 +1,15 @@
+package com.avalialivros.m3s04.operations;
+
+import com.avalialivros.m3s04.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EmailNotification implements Notification {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailNotification.class);
+
+    @Override
+    public void send(Person person, String content) {
+        LOGGER.info("Notification for {}: {}", person.getEmail(), content);
+    }
+}
